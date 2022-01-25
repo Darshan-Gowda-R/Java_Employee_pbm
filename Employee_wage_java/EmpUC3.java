426c797a4227dad01eb2cd32e9aef781d018b4df
darshan <darshan.gowda.r.100@gmail.com>
2022-01-25 08:37:33 +0530

    Add Part time Employee & Wage

---
+++ Employee_wage_java/EmpUC3.java
@@ -0,0 +1,27 @@
+public class EmpUC4{
+
+	public static final int EMP_WAGE_PER_HOUR=20;
+	public static final int EMP_DAY_HOUR=8;
+	public static final int PART_TIME_HOUR=4;
+	public static final int ABSENT=0;
+	public static final int PART_TIME=1;
+	public static final int FULL_TIME=2;
+	public static void main(String[] args){
+
+		int empCheck=(int)Math.floor(Math.random()*10)%3;
+		if(empCheck==FULL_TIME)
+			System.out.println("Employee wage : "+(EMP_WAGE_PER_HOUR*EMP_DAY_HOUR));
+		else if (empCheck==PART_TIME)
+			System.out.println("Employee wage : "+(EMP_WAGE_PER_HOUR*PART_TIME_HOUR));
+		else
+			System.out.println("Employee WAGE : 0" );
+	}
+}
+
+
+
+
+
+
+
+
